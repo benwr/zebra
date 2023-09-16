60fa1df49208e98a017a88e7a5df227e8b61e19f
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 05:52:39 -0700

    working tabbed UI; empty tabs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,23 @@ fn main() {
     dioxus_desktop::launch(App);
 }
 
+#[derive(Clone, Copy, Debug)]
+enum ActiveTab {
+    MyKeys,
+    OtherKeys,
+    Sign,
+    Verify,
+    About,
+}
+
 fn App(cx: Scope) -> Element {
+    use dioxus_desktop::tao::dpi::{LogicalSize, Size};
+    let desktop = dioxus_desktop::use_window(cx);
+    desktop.set_title("Spartacus");
+    desktop.set_min_inner_size(Some(Size::Logical(LogicalSize{width: 640.0, height: 256.0})));
+
+    use_shared_state_provider(cx, || ActiveTab::MyKeys);
+
     cx.render(rsx! {
         section {
             class: "spartacus",
@@ -14,36 +30,160 @@ fn App(cx: Scope) -> Element {
             TabSelect {}
             div {
                 class: "contents",
-                About {}
+                {
+                    match *use_shared_state::<ActiveTab>(cx).unwrap().read() {
+                        ActiveTab::MyKeys => rsx! { MyKeys {} },
+                        ActiveTab::OtherKeys => rsx! { OtherKeys {} },
+                        ActiveTab::Sign => rsx! { Sign {} },
+                        ActiveTab::Verify => rsx! { Verify {} },
+                        ActiveTab::About => rsx! { About {} },
+                    }
+                }
             }
         }
     })
 }
 
 fn TabSelect(cx: Scope) -> Element {
+    let desktop = dioxus_desktop::use_window(cx);
+    let active_tab = *use_shared_state::<ActiveTab>(cx).unwrap().read();
     cx.render(rsx! {
         nav {
+            onmousedown: move |_| { desktop.drag(); },
             class: "tab_select",
             div {
-                class: "tab_choice inactive_tab",
-                "My Keys"
+                onclick: move |_| {*use_shared_state::<ActiveTab>(cx).unwrap().write() = ActiveTab::MyKeys},
+                class: {
+                    if let ActiveTab::MyKeys = active_tab {
+                        "tab_choice active_tab"
+                    } else {
+                        "tab_choice inactive_tab"
+                    }
+                },
+                "My Keypairs"
             }
             div {
-                class: "tab_choice inactive_tab",
+                onclick: move |_| {*use_shared_state::<ActiveTab>(cx).unwrap().write() = ActiveTab::OtherKeys},
+                class: {
+                    if let ActiveTab::OtherKeys = active_tab {
+                        "tab_choice active_tab"
+                    } else {
+                        "tab_choice inactive_tab"
+                    }
+                },
                 "Other Keys"
             }
             div {
+                onclick: move |_| {*use_shared_state::<ActiveTab>(cx).unwrap().write() = ActiveTab::Sign},
+                class: {
+                    if let ActiveTab::Sign = active_tab {
+                        "tab_choice active_tab"
+                    } else {
+                        "tab_choice inactive_tab"
+                    }
+                },
                 class: "tab_choice inactive_tab",
                 "Sign"
             }
             div {
+                onclick: move |_| {*use_shared_state::<ActiveTab>(cx).unwrap().write() = ActiveTab::Verify},
+                class: {
+                    if let ActiveTab::Verify = active_tab {
+                        "tab_choice active_tab"
+                    } else {
+                        "tab_choice inactive_tab"
+                    }
+                },
                 class: "tab_choice inactive_tab",
                 "Verify"
             }
             div {
-                class: "tab_choice active_tab",
+                onclick: move |_| {*use_shared_state::<ActiveTab>(cx).unwrap().write() = ActiveTab::About},
+                class: {
+                    if let ActiveTab::About = active_tab {
+                        "tab_choice active_tab"
+                    } else {
+                        "tab_choice inactive_tab"
+                    }
+                },
                 "About"
             }
+        }
+    })
+}
+
+fn MyKeys(cx: Scope) -> Element {
+    cx.render(rsx! {
+        table {
+            class: "mykeys",
+            thead {
+                tr {
+                    th {
+                        "Fingerprint"
+                    }
+                    th {
+                        "Name"
+                    }
+                    th {
+                        "Email"
+                    }
+                    th {
+                        "Actions"
+                    }
+                }
+            }
+            tbody {
+                tr {
+                    td {
+                        class: "fingerprint",
+                        "jf^:GW)T=&^}}dg-$6VVm"
+                    }
+                    td {
+                        class: "name",
+                        "Ben Weinstein-Raun"
+                    }
+                    td {
+                        class: "email",
+                        "b@w-r.me"
+                    }
+                    td {
+                        class: "actions",
+                        button {
+                            "Copy Public Key",
+                        }
+                        button {
+                            "Send To New Device"
+                        }
+                        button {
+                            "Delete",
+                        }
+                    }
+                }
+            }
+        }
+        button {
+            "Create New Keypair"
+        }
+    })
+}
+
+fn OtherKeys(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+        }
+    })
+}
+
+fn Sign(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+        }
+    })
+}
+
+fn Verify(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
         }
     })
 }
@@ -53,7 +193,7 @@ fn About(cx: Scope) -> Element {
         div {
             class: "about",
             h1 {"Spartacus"}
-            p {"A tool for ring signatures."}
+            p {"A tool for creating and verifying ring signatures."}
             p {"Version 0.0.0"}
         }
     })
