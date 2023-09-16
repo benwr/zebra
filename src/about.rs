8a4a871ad52672ddb42ce20eea9034dfb0f586e6
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 16:01:17 -0700

    wip: Fix copyright info in about page (issue

--- src/about.rs
+++ src/about.rs
@@ -27,7 +27,7 @@ pub fn About(cx: Scope) -> Element {
             }
             p {
                 class: "copyright_info",
-                "All other content is written by Ben Weinstein-Raun, and copyright of Kurt Brown 2023. Source code available at "
+                "All other content is written by Ben Weinstein-Raun; copyright assigned to Kurt Brown. Source code available at "
                 a {
                     href: "https://github.com/benwr/spartacus",
                     "https://github.com/benwr/spartacus"
