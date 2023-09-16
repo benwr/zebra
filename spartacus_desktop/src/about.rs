0701f12a67da9c3be39b0b5fe86cc2d2bab0819c
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 16:42:41 -0700

    Rearrange as a workspace

--- spartacus_desktop/src/about.rs
+++ spartacus_desktop/src/about.rs
@@ -10,14 +10,14 @@ pub fn About(cx: Scope) -> Element {
             p {"Version 0.0.0"}
             p {"A tool for creating and verifying ring signatures."}
             img {
-                src: "../spartacus_head.png",
+                src: "/spartacus_desktop/spartacus_head.png",
                 height: "256px",
                 width: "256px",
             }
             p {
                 class: "copyright_info",
                 "App Logo adapted from "
-                a { 
+                a {
                     href: "https://www.flickr.com/photos/carolemage/8270400666",
                     "Spartacus, marble sculpture of Denis Foyatier (1830), Louvre Museum"
                 }
