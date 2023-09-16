b9313ead8c33f4f767eeee3e95b91eedd6cd9920
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 15:16:02 -0700

    wip: Add copyright notice

--- src/about.rs
+++ src/about.rs
@@ -12,8 +12,10 @@ pub fn About(cx: Scope) -> Element {
                 height: "256px",
                 width: "256px",
             }
+            p {"(App Logo adapted from )"}
             p {"A tool for creating and verifying ring signatures."}
             p {"Version 0.0.0"}
+            p {"Copyright 2023 Ben Weinstein-Raun"}
         }
     })
 }
