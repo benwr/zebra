190b4d435f0626d248f33d5b953c012c17b1d38b
Ben Weinstein-Raun <b@w-r.me>
2023-09-15 23:06:26 -0700

    basic About page and tabs that look... basically passable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use dioxus::prelude::{rsx, Scope, Element, dioxus_elements};
+use dioxus::prelude::*;
 
 fn main() {
     dioxus_desktop::launch(App);
@@ -8,6 +8,53 @@ fn main() {
 
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div { "SPARTACVSSVM" }
+        section {
+            class: "spartacus",
+            style { include_str!("style.css") }
+            TabSelect {}
+            div {
+                class: "contents",
+                About {}
+            }
+        }
+    })
+}
+
+fn TabSelect(cx: Scope) -> Element {
+    cx.render(rsx! {
+        nav {
+            class: "tab_select",
+            div {
+                class: "tab_choice inactive_tab",
+                "My Keys"
+            }
+            div {
+                class: "tab_choice inactive_tab",
+                "Other Keys"
+            }
+            div {
+                class: "tab_choice inactive_tab",
+                "Sign"
+            }
+            div {
+                class: "tab_choice inactive_tab",
+                "Verify"
+            }
+            div {
+                class: "tab_choice active_tab",
+                "About"
+            }
+        }
+    })
+}
+
+fn About(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "about",
+            h1 {"Spartacus"}
+            p {"A tool for ring signatures."}
+            p {"Version 0.0.0"}
+        }
     })
 }
