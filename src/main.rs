6d2fd602a2404d50b2a45de9e0e34402d6042390
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 04:08:26 -0700

    wip: Rename module to match canonical name

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
+    use dioxus_desktop::tao::dpi::{LogicalSize, Size};
     let desktop = dioxus_desktop::use_window(cx);
     desktop.set_title("Spartacus");
-    desktop.set_min_inner_size(Some(dioxus_desktop::tao::dpi::Size::Logical(LogicalSize{width: 512.0, height: 256.0})));
+    desktop.set_min_inner_size(Some(Size::Logical(LogicalSize{width: 512.0, height: 256.0})));
     cx.render(rsx! {
         section {
             class: "spartacus",
