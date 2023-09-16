d820d6cc7c75665cdcfbde0863fe4307469f8065
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 07:21:56 -0700

    wip: update about

--- src/main.rs
+++ src/main.rs
@@ -374,6 +374,8 @@ fn About(cx: Scope) -> Element {
             h1 {"Spartacus"}
             img {
                 src: "../spartacus_head.png",
+                height: "256px";
+                width: "256px";
             }
             p {"A tool for creating and verifying ring signatures."}
             p {"Version 0.0.0"}
