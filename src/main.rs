ecee4a7d0269b833b00fdeee7445b64d46f0f95c
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 15:11:29 -0700

    wip: Remove About function

--- src/main.rs
+++ src/main.rs
@@ -373,19 +373,3 @@ fn Verify(cx: Scope) -> Element {
         }
     })
 }
-
-fn About(cx: Scope) -> Element {
-    cx.render(rsx! {
-        div {
-            class: "about",
-            h1 {"Spartacus"}
-            img {
-                src: "../spartacus_head.png",
-                height: "256px",
-                width: "256px",
-            }
-            p {"A tool for creating and verifying ring signatures."}
-            p {"Version 0.0.0"}
-        }
-    })
-}
