ea5973a1120c90a1571ebfe4b60bee5ac3714785
Ben Weinstein-Raun <b@w-r.me>
2023-09-16 15:17:22 -0700

    wip: Add flickr photo attribution to about page.

--- src/about.rs
+++ src/about.rs
@@ -12,7 +12,7 @@ pub fn About(cx: Scope) -> Element {
                 height: "256px",
                 width: "256px",
             }
-            p {"(App Logo adapted from )"}
+            p {"(App Logo adapted from" a { href: "https://www.flickr.com/photos/carolemage/8270400666", "Spartacus, marble sculpture of Denis Foyatier (1830), Louvre Museum"} ")"}
             p {"A tool for creating and verifying ring signatures."}
             p {"Version 0.0.0"}
             p {"Copyright 2023 Ben Weinstein-Raun"}
