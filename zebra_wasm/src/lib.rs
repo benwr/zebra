501c382285db4aeabf0186acfd28f545349e1906
Simon Baars <simon.mailadres@gmail.com>
2024-06-30 18:12:03 -0700

    Basic signed message functionality

--- zebra_wasm/src/lib.rs
+++ zebra_wasm/src/lib.rs
@@ -1,13 +1,15 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use zebra_crypto::{PublicKey, Signature, Scalar, RistrettoPoint, SignedMessage};
+use std::str::FromStr;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, zebra_wasm!");
+pub fn verify_signature(message: &str) -> bool {
+    match SignedMessage::from_str(message) {
+        Ok(signed_message) => signed_message.verify(),
+        Err(_) => false,
+    }
 }
