6c8d74e3dbbc79071086cabd4113cf60e84f3f23
Simon Baars <simon.mailadres@gmail.com>
2024-06-30 19:27:30 -0700

    bind function to html button

--- zebra_webapp/bootstrap.js
+++ zebra_webapp/bootstrap.js
@@ -2,4 +2,7 @@
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
 import("./index.js")
+  .then(module => {
+    window.verifySignature = module.verifySignature;
+  })
   .catch(e => console.error("Error importing `index.js`:", e));
