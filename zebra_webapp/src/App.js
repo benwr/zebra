080dce06686d52b8f9b44a4eb452157ced2c8d6c
Simon Baars <simon.mailadres@gmail.com>
2024-06-30 18:45:35 -0700

    simplify app deliverable

--- zebra_webapp/src/App.js
+++ zebra_webapp/src/App.js
@@ -1,52 +1,9 @@
 import React, { useEffect } from "react";
-import nacl from "tweetnacl";
-import naclUtil from "tweetnacl-util";
-import crypto from "crypto-browserify";
-import testMessage from "./message";
-import testMessage3 from "./message3";
-
-function hashMessageAndRing(message, keys) {
-  const hash = crypto.createHash('sha512');
-  hash.update(message);
-  keys.forEach(key => {
-      hash.update(key);
-  });
-  return hash;
-}
-
-class Signature {
-  constructor(challenge, ringResponses) {
-      this.challenge = challenge;
-      this.ringResponses = ringResponses;
-  }
-
-  verify(message) {
-      const initialHash = hashMessageAndRing(message, this.ringResponses.map(([k, _]) => k));
-      let reconstructedChallenge = this.challenge;
-
-      for (const [keypoint, response] of this.ringResponses) {
-          const h = initialHash.copy();
-          const hashUpdate = nacl.scalarMult.base(response).map((v, i) => v + nacl.scalarMult(reconstructedChallenge, keypoint)[i]);
-          h.update(hashUpdate);
-          reconstructedChallenge = naclUtil.decodeBase64(h.digest('base64'));
-      }
-
-      return naclUtil.encodeBase64(this.challenge) === naclUtil.encodeBase64(reconstructedChallenge);
-  }
-}
-
-function testEncrypt() {
-  const message = naclUtil.decodeUTF8("example message");
-  const challenge = new Uint8Array(32);  // Replace with actual challenge
-  const ringResponses = [[new Uint8Array(32), new Uint8Array(32)]];  // Replace with actual ring responses
-  
-  const signature = new Signature(challenge, ringResponses);
-  console.log(signature.verify(message));
-}
+import * as wasm from "./pkg/zebra_wasm_bg"
 
 function App() {
   useEffect(() => {
-    testMessage3();
+    wasm.greet();
   }, []);
   return (
     <div>
